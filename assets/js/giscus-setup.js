41260fd23a3a704bd756f9066d4b6863af6fac6a
desifisker <desifisker@users.noreply.github.com>
2025-12-30 03:04:22 +0000

    Deploying to gh-pages from @ desifisker/desifisker.github.io@3759b817191f38855912b00aa83a30f36969e887 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "desifisker/desifisker-desifisker.github.io",
+    "data-repo": "desifisker/desifisker.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
